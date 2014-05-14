7c200c2369d632b1f2e6ffdc54ad3a6224ef0b37
Michael Tseng <matseng@gmail.com>
2014-05-14 10:30:38 -0700

    Completed thru adding task via modal window and controller

--- www/js/app.js
+++ www/js/app.js
@@ -7,4 +7,31 @@ angular.module('todo', ['ionic'])
     { title: 'Get high enough to grab the flag' },
     { title: 'Find the Princess' }
   ];
+
+  // Create and load the Modal
+  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+    $scope.taskModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+  // Called when the form is submitted
+  $scope.createTask = function(task) {
+    $scope.tasks.push({
+      title: task.title
+    });
+    $scope.taskModal.hide();
+    task.title = "";
+  };
+
+  // Open our new task modal
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  };
+
+  // Close the new task modal
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
+  };
 });
